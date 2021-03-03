7b6c6984c1f382251b68e2333f49c736c1ab3d0b
zjw981119 <962610351@qq.com>
2021-03-03 16:38:13 +0800

    完成登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Icon, Message } from 'element-ui'
+
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Icon)
+Vue.prototype.$message = Message
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
+Vue.use(Router)
 
-Vue.use(VueRouter)
+const router = new Router({
+  routes:[
+    {path:'/', redirect: '/login'},
+    {path: '/login', component: Login},
+    {path: '/home', component: Home}
+  ]
+})
 
-const routes = [
-  
-]
+//挂载路由导航守卫
+router.beforeEach((to,from,next) => {
+    //to 将要访问的路径
+    //from 从哪个路径而来
+    //next 函数，放行 
+    //next() 放行  next('/login')强制跳转
 
-const router = new VueRouter({
-  routes
+    if(to.path === '/login') return next();
+    //获取token
+    const tokenStr= window.sessionStorage.getItem('token')
+    //未获得token,强制跳转
+    if(!tokenStr) return next('/login')
+    next()
 })
 
 export default router
+
