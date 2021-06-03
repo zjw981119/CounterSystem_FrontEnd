a31f0e933419fef6fce3b06eb9784918f9598109
tianyusai <360978335@qq.com>
2021-06-03 17:34:13 +0800

    挖机配置表&生产情况表

--- src/api/DiggerProduction.js
+++ src/api/DiggerProduction.js
@@ -1,25 +1,25 @@
 import request from '@/utils/request'
 
-export function queryConditionalConfig(data){
+export function queryConditionalProduction(data){
   return request({
-    url: './diggerConfig/queryConditional',
+    url: './diggerProduction/queryConditional',
     method: 'post',
     //headers: { 'content-type': 'application/json' },
     data: data,
   })
 }
 
-export function delDiggerConfig(data){
+export function deleteDiggerProduction(data){
   return request({
-    url: './diggerConfig/delDiggerConfigById'+"/"+data,
+    url: './diggerProduction/deleteDiggerProduction'+"/"+data,
     method: 'post',
     //headers: { 'content-type': 'application/json' },
   })
 }
 
-export function editDiggerConfig(data){
+export function updateDiggerProduction(data){
   return request({
-    url: './diggerConfig/editDiggerConfig',
+    url: './diggerProduction/updateDiggerProduction',
     method: 'post',
     //headers: { 'content-type': 'application/json' },
     data: data
